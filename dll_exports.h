37f472d4cba146130be55a1e99c28b4fd184ab5a
Daniel Miller (Earthmark) <earthmark.miller@gmail.com>
2021-11-04 21:32:23 -0700

    Fixed the wrong macro being used.

--- dll_exports.h
+++ dll_exports.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#ifdef MSDFGEN_DLL
+#ifdef MSDFGEN_BUILD_SHARED_LIBRARY
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32__) || defined(__NT__)
 #define DLL_EXPORT extern "C" __declspec(dllexport)
 #define DLL_API __cdecl
